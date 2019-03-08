3db405cf75c46bd8bb8cc88e23dc1c8527cb5329
Maghfieggy <eggydj@gmail.com>
2019-03-08 09:48:26 +0700

    string 1-4

---
+++ string4.js
@@ -0,0 +1,19 @@
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = word.substring(3, 14 ); 
+var thirdWord3 = word.substring(15, 18);
+var fourthWord3 = word.substring(18, 20); 
+var fifthWord3 =word.substring(21, 25);
+
+var FirstWordLength = exampleFirstWord3.length;
+var secondWord4 = secondWord3.length;
+var thirdWord4 = thirdWord3.length;
+var fourthWord4 = fourthWord3.length;
+var fifthWord4 = fifthWord3.length;
+
+
+console.log('First Word: ' + exampleFirstWord3 +' '+ 'With Length' +' ' + FirstWordLength);
+console.log('Second Word: ' + secondWord3+' '+ 'With Length' +' ' + secondWord4);
+console.log('Third Word: ' + thirdWord3+' '+ 'With Length' +' ' + thirdWord4);
+console.log('Fourth Word: ' + fourthWord3+' '+ 'With Length' +' ' + fourthWord4);
+console.log('Fifth Word: ' + fifthWord3+' '+ 'With Length' +' ' + fifthWord4);
